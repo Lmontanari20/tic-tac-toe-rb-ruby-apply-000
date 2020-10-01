3e1f61a8cd1555844b000915f3b2e3b0036642c0
Lucas Montanari <lmonty2011@gmail.com>
2020-10-01 21:25:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,3 +47,79 @@ def turn(board, character)
     turn(board,character)
   end
 end
+
+def turn_count(board)
+  count = 0
+  board.each do |tile|
+    if tile == "X" || tile == "O"
+      count += 1
+    end
+  end
+  count
+end
+
+def current_player(board)
+  count = turn_count(board)
+  count % 2 == 0? "X" : "O"
+end
+
+def won(board)
+  WIN_COMBINATIONS.each do |combo|
+    index1 == combo[0]
+    index2 == combo[1]
+    index3 == combo[2]
+    
+    if (board[index1] == "X" && board[index2] == "X" && board[index3] == "X")
+      return combo
+    elsif (board[index1] == "O" && board[index2] == "O" && board[index3] == "O")
+      return combo
+    end
+  end
+  return false
+end
+
+def full?(board)
+  board.each do |tile|
+    if tile == "" || tile == " " || tile == nil
+      return false
+    end
+  end
+  return true
+end
+
+def draw?(board)
+  if !won(board) && full?(board)
+    return true
+  end
+  false
+end
+
+def over?(board)
+  if draw?(board) || full?(board) || won(board)
+    return true
+  end
+  false
+end
+
+def winner(board)
+  index = won(board)
+  if index != false
+    return board[index[0]]
+  end
+  return false
+end
+
+def play(board)
+  while !over?(board)
+    count = turn_count(board)
+    char = count % 2 == 0 ? "X" : "O"
+    turn(board, char)
+  end
+  
+  if won(board)
+    puts "Congratulations to the winner #{winner(board)}"
+  else 
+    puts "The game ended up in a draw"
+  end
+end
+
